407968b8c582995af48ade33a17e738660cf00ee
Paulo Freire <paulo.freiire@hotmail.com>
2020-06-23 17:07:30 -0300

    added User model

--- src/database/migrations/20200623183342-create-users.js
+++ src/database/migrations/20200623183342-create-users.js
@@ -15,6 +15,11 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
       password_hash: {
         type: DataTypes.STRING,
         allowNull: false,
