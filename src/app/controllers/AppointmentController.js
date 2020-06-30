32b3eb72c8a16d1e905e0f05b057022a90631e2e
Paulo Freire <paulo.freiire@hotmail.com>
2020-06-30 20:09:21 -0300

    Added validation date in AppointmentController

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import Appointment from "../models/Appointment";
+import {startOfHour, parseISO, isBefore} from 'date-fns';
+import User from "../models/User";
 import * as Yup from 'yup';
 
 class AppointmentController {
@@ -14,20 +16,41 @@ class AppointmentController {
 
         const {provider_id, date} = req.body;
 
-        /**
-         * Check if provider_id is a provider
-         */
-
-        const isProvider = await User.findOne(({
+        const isProvider = await User.findOne({
             where: {id: provider_id, provider: true}
-        }));
+        });
 
+        /*Check if provider_id is a provider*/
         if (!isProvider) {
             return res.status(401).json({error: 'You can only create appointments with providers'})
         }
 
+        const hourStart = startOfHour(parseISO(date));
 
-        return res.json();
+        /*Check for past dates*/
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({error: 'Paste dates are not permitted'})
+        }
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            }
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({error: 'Appointment date is not available'});
+        }
+
+        const appointment = await Appointment.create({
+            user_id: req.userId,
+            provider_id,
+            date
+        });
+
+        return res.json(appointment);
     }
 }
 
