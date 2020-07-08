2da81f8e3f5ff01789c62151435440ab6f749c31
Paulo Freire <paulo.freiire@hotmail.com>
2020-07-08 13:01:16 -0300

    added libs

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,9 +6,7 @@ import Appointment from "../models/Appointment";
 import User from "../models/User";
 import File from "../models/File";
 import Notification from "../schemas/Notification";
-
 import Mail from "../../lib/Mail";
-import {DataTypes} from "sequelize";
 
 
 class AppointmentController {
@@ -26,7 +24,7 @@ class AppointmentController {
                 include: [{
                     model: File,
                     as: 'avatar',
-                    attributes: ['id', 'path', 'url']
+                    attributes: ['id', 'path', 'url'],
                 }]
             }]
         });
@@ -107,9 +105,16 @@ class AppointmentController {
                     model: User,
                     as: 'provider',
                     attributes: ['name', 'email']
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name']
                 }
             ]
         });
+
+        console.log(appointment);
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).js({error: "you don't have permission to cancel this appointment"});
@@ -128,11 +133,22 @@ class AppointmentController {
 
         await appointment.save();
 
-        /*await Mail.sendMail({
+        console.log(appointment);
+
+        await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento cancelado',
-            text: 'Você tem um novo cancelamento'
-        });*/
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "dd 'de' MMM', às' H:mm'h",
+                    {locale: pt}
+                )
+            },
+        });
 
         return res.json(appointment);
     }
--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,8 +1,11 @@
 import nodemailer from 'nodemailer';
+import {resolve} from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 import mailConfig from "../config/mail";
 
 class Mail {
-    constructor(props) {
+    constructor() {
         const {host, port, secure, auth} = mailConfig;
 
         this.transporter = nodemailer.createTransport({
@@ -11,6 +14,8 @@ class Mail {
             secure,
             auth: auth.user ? auth : null
         });
+
+        this.configureTemplates();
     }
 
     sendMail(message) {
@@ -18,6 +23,21 @@ class Mail {
             ...mailConfig.default,
             ...message
         });
+    }
+
+    configureTemplates() {
+        const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+        this.transporter.use('compile', nodemailerhbs({
+            viewEngine: exphbs.create({
+                layoutsDir: resolve(viewPath, 'layouts'),
+                partialsDir: resolve(viewPath, 'partials'),
+                defaultLayout: 'default',
+                extname: '.hbs'
+            }),
+            viewPath,
+            extNme: '.hbs'
+        }));
     }
 }
 
